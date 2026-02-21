6e0e29a621a065d8628fc059579391c37ad39b15
xavier whitlow <xwhitlow00@gmail.com>
2026-02-20 22:53:58 -0600

    a

--- csci2010/assignment2/SubsitutionCipher.java
+++ csci2010/assignment2/SubsitutionCipher.java
@@ -37,7 +37,7 @@ public abstract class SubsitutionCipher implements Cipher{
         }
         for (char c : upper) {
             if (c == ch) {
-                return ch - 'M';
+                return ch - 'A';
             }
         }
         return -1;
